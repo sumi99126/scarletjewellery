81f702f17bdf9833edd0124f6b762ef257536a1e
Sumera Sikandari <sumi9916@gmail.com>
2026-08-02 22:58:41 +0500

    Update project

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { WizzTechProtectionProvider } from '@wizztech/protection'
+import '@wizztech/protection/dist/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WizzTechProtectionProvider platformUrl="https://wizztech-demo-website-platform.netlify.app">
+      <App />
+    </WizzTechProtectionProvider>
   </React.StrictMode>,
 )
